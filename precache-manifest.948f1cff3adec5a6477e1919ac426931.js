95feb97416ad2e091b6240368e382807aa911fff
arvind <arvind.bisht.90475@gmail.com>
2020-10-13 21:14:45 +1000

    Updates

---
+++ precache-manifest.948f1cff3adec5a6477e1919ac426931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b213d653a1d85727a920d2c590f7e20",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "35bc2ed35d474a4192a4",
+    "url": "/React-todo/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "2cc03c440bf286072e10",
+    "url": "/React-todo/static/js/2.c068cca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo/static/js/2.c068cca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35bc2ed35d474a4192a4",
+    "url": "/React-todo/static/js/main.bec33292.chunk.js"
+  },
+  {
+    "revision": "3bf2ba0de9f90277f1a8",
+    "url": "/React-todo/static/js/runtime-main.b79d70a9.js"
+  }
+]);
